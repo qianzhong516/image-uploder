be847c259af7c4c12750bb612b958c9feb13b3db
qianzhong516 <zhongqian516@gmail.com>
2024-06-26 16:14:07 +0200

    add modal

---
+++ src/components/modal/modal.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Modal from './modal';
+import { fn } from '@storybook/test';
+import Button from '@/components/button/button';
+
+const meta = {
+    title: 'UI/Modal',
+    component: Modal,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    args: {
+        onClose: fn()
+    },
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const View: Story = {
+    args: {
+        title: 'Upload image(s)',
+        subtitle: 'You may upload up to 5 images',
+        children: <div className='bg-[#ccc] w-full h-[300px] p-2'>Modal Content</div>,
+        className: 'min-w-[600px]',
+        leftButton: ({ className }) => <Button theme='secondary' className={className}>Cancel</Button>,
+        rightButton: ({ className }) => <Button theme='secondary' className={className} disabled>Select Image</Button>
+    }
+}
---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,44 @@
+import Button from '@/components/button/button';
+import CloseIcon from '@/components/icons/close';
+import clsx from 'clsx';
+
+type ModalProps = {
+    title: string,
+    subtitle: string,
+    children: React.ReactNode,
+    leftButton: ({ className }: { className: string }) => JSX.Element,
+    rightButton: ({ className }: { className: string }) => JSX.Element,
+    className?: string,
+    onClose: () => void,
+}
+
+export default function Modal({
+    title,
+    subtitle,
+    children,
+    leftButton,
+    rightButton,
+    className,
+    onClose,
+}: ModalProps) {
+    const buttonStyles = 'text-sm px-0 py-0';
+
+    return (
+        <div className={clsx('flex flex-col gap-6 p-4 shadow-md', className)}>
+            <div className='flex justify-between w-full'>
+                <div className='flex flex-col gap'>
+                    <h1 className="text-lg">{title}</h1>
+                    <p className="text-sm text-neutral-400">{subtitle}</p>
+                </div>
+                <Button theme='tertiary' onClick={onClose}><CloseIcon /></Button>
+            </div>
+            <div>
+                {children}
+            </div>
+            <div className='flex justify-between gap-4'>
+                {leftButton({ className: buttonStyles })}
+                {rightButton({ className: buttonStyles })}
+            </div>
+        </div>
+    );
+}
