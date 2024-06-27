c5f93d534ad7e1242a9611044ceb66f8602d9292
qianzhong516 <zhongqian516@gmail.com>
2024-06-27 03:08:40 +0200

    update global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      textColor: {
+        primary: '#171717',
+        'primary-hover': '#0a0a0a',
+        'primary-invert': '#fff',
+        secondary: '#525252',
+        tertiary: '#737373',
+        brand: '#4338ca',
+        disabled: '#a3a3a3',
+        error: '#dc2626',
+        'error-emphasize': '#991b1b',
+        success: '#15803d',
+        warning: '#a16207',
       },
     },
   },
