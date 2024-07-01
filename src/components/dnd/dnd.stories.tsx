660dce1c4c2b209b673952044016ec1835bf63e4
qianzhong516 <zhongqian516@gmail.com>
2024-07-01 15:05:07 +0200

    add dnd handlers

--- src/components/dnd/dnd.stories.tsx
+++ src/components/dnd/dnd.stories.tsx
@@ -1,14 +1,27 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Dnd from './dnd';
+import { fn } from '@storybook/test';
 
 const meta = {
     title: 'UI/DragAndDrop',
     component: Dnd,
     tags: ['autodocs'],
+    args: {
+        uploadFiles: fn()
+    }
 } satisfies Meta<typeof Dnd>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const View: Story = {
+export const AllowMultipleUploads: Story = {
+    args: {
+        multiple: true
+    }
+}
+
+export const AllowSingleUpload: Story = {
+    args: {
+        multiple: false
+    }
 }
--- src/components/dnd/dnd.tsx
+++ src/components/dnd/dnd.tsx
@@ -1,11 +1,42 @@
 import UploadCloudIcon from '@/components/icons/upload-cloud';
+import { DragEvent } from 'react';
 
-export default function Dnd() {
+type DndProps = {
+    multiple: boolean,
+    uploadFiles: (files: File[]) => Promise<void>,
+}
+
+export default function Dnd({
+    multiple,
+    uploadFiles
+}: DndProps) {
+    const handleOnDragOver = (e: DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+    }
+
+    const handleOnDrop = (e: DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+
+        if (e.dataTransfer.items) {
+            Array.from(e.dataTransfer.items).forEach((item, i) => {
+                if (item.kind === 'file') {
+                    const file = item.getAsFile();
+                    console.log(`file[${i}].name = ${file?.name}`);
+                }
+            });
+        } else {
+            // Use DataTransfer interface to access the file(s)
+            Array.from(e.dataTransfer.files).forEach((file, i) => {
+                console.log(`file[${i}].name = ${file.name}`);
+            });
+        }
+    }
+
     return (
-        <form className="flex flex-col gap-4 justify-center items-center p-4 bg-neutral-50 border-2 border-neutral-200 rounded">
+        <div className="flex flex-col gap-4 justify-center items-center p-4 bg-neutral-50 border-2 border-neutral-200 rounded" onDragOver={handleOnDragOver} onDrop={handleOnDrop}>
             <div>
                 <label htmlFor="profile-icon-uploader" className='block text-indigo-500 rounded-full p-2 cursor-pointer shadow focus-within:outline focus-within:outline-2'>
-                    <input type="file" name="profile-icon" id="profile-icon-uploader" className='visuallyhidden' />
+                    <input type="file" name="profile-icon" id="profile-icon-uploader" className='visuallyhidden' multiple={multiple} />
                     <UploadCloudIcon size={25} />
                 </label>
             </div>
@@ -14,6 +45,6 @@ export default function Dnd() {
                 <h2 className="text-md font-semibold">Click or drag and drop to upload</h2>
                 <p className="text-neutral-600 text-sm">PNG, or JPG (Max 5MB)</p>
             </div>
-        </form>
+        </div>
     )
 }
