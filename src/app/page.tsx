c5f4c7babb3ae142375dd28dbd708698126d189b
qianzhong516 <zhongqian516@gmail.com>
2024-07-03 21:28:30 +0200

    demo wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,100 @@ import ProfileBanner from '@/components/profile_banner/profile_banner';
 import Image from 'next/image';
 import UploadImageModal from '@/components/upload_image_modal/upload_image_modal';
 import { useCallback, useState } from 'react';
-import { ImageItemProps } from '@/components/image_list/image_item';
+import axios, { AxiosProgressEvent } from 'axios';
+import { getImgSrc, getTotalSize } from '@/utils';
+import { ImageListProps } from '@/components/image_list/image_list';
+
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
-  const [imageListMap, setImageListMap] = useState<Map<string, {
-    id: string,
-  } & ImageItemProps>>(new Map());
+  const [imageList, setImageList] = useState<ImageListProps>([]);
 
-  const hadnleUpdatePicture = useCallback(() => setIsOpen(true), []);
+  const handleUpdatePicture = useCallback(() => setIsOpen(true), []);
   const handleOnClose = useCallback(() => setIsOpen(false), []);
   const handleUploadImage = async (files: File[]) => {
+    // append pending processes in UI
+    const sources = await getImgSrc(files);
+    const imagesInUpload: ImageListProps = files.map((file, i) => {
+      return {
+        id: file.name,
+        title: file.name,
+        imgSrc: sources[i] as string,
+        totalSize: getTotalSize(file.size),
+        state: 'loading',
+        progress: 0,
+        onCancelUpload: () => { } // TODO:
+      }
+    });
+    setImageList(prev => [...prev, ...imagesInUpload]);
 
+    // upload files
+    files.forEach((file, i) => {
+      const formData = new FormData();
+      formData.append('file', file);
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        onUploadProgress: (progressEvent: AxiosProgressEvent) => {
+          const { loaded, total } = progressEvent;
+          if (!total) { return }
+          console.log(Math.round(loaded / total * 100))
+          // use setState callback here to avoid stale state
+          setImageList(prev => {
+            const updated = [...prev];
+            const item = updated[i];
+            if (item.state === 'loading') {
+              item.progress = Math.round(loaded / total * 100);
+            }
+            return updated;
+          });
+        },
+
+        onerror: () => {
+
+        }
+      }
+
+      axios.post('http://localhost:3000/api/upload', formData, config).then(res => {
+        setImageList(prev => {
+          const updated = [...prev];
+          const item = updated[i];
+          if (item.state === 'loading') {
+            updated.splice(i, 1, {
+              state: 'load-success',
+              id: item.title,
+              title: item.title,
+              totalSize: item.totalSize,
+              imgSrc: item.imgSrc,
+              onDelete: () => { } // TODO:
+            });
+          }
+          return updated
+        });
+
+        setTimeout(() => {
+          setImageList(prev => {
+            const updated = [...prev];
+            const item = updated[i];
+            if (item.state === 'load-success') {
+              updated.splice(i, 1, {
+                state: 'complete',
+                id: item.title,
+                title: item.title,
+                totalSize: item.totalSize,
+                imgSrc: item.imgSrc,
+                onCropImage: () => { }, // TODO:
+                onDelete: () => { } // TODO:
+              });
+            }
+            return updated;
+          })
+        }, 2000);
+      }).catch(err => {
+        console.log(err);
+      });
+    });
   };
 
   return (
@@ -29,11 +111,11 @@ export default function Home() {
         pronounce='He/Him'
         nation='Canada'
         city='Vancouver'
-        onUpdatePicture={hadnleUpdatePicture} />
+        onUpdatePicture={handleUpdatePicture} />
       {isOpen && <UploadImageModal
         open={isOpen}
         allowMutiple={true}
-        imageList={Array.from(imageListMap.values())}
+        imageList={Array.from(imageList.values())}
         imageLimit={5}
         uploadFiles={handleUploadImage}
         onClose={handleOnClose}
