db9a85feb52858eb284a4b708ae70f53c98a7390
qianzhong516 <zhongqian516@gmail.com>
2024-07-05 16:28:36 +0200

    use immer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ import { useCallback, useState } from 'react';
 import axios, { AxiosError, AxiosProgressEvent } from 'axios';
 import { getImgSrc, getTotalSize } from '@/utils';
 import { ImageListProps } from '@/components/image_list/image_list';
-
+import { useImmer } from "use-immer";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
-  const [imageList, setImageList] = useState<ImageListProps>([]);
+  const [imageList, setImageList] = useImmer<ImageListProps>([]);
 
   const handleUpdatePicture = useCallback(() => setIsOpen(true), []);
   const handleOnClose = useCallback(() => setIsOpen(false), []);
@@ -29,7 +29,7 @@ export default function Home() {
         onCancelUpload: () => { } // TODO:
       }
     });
-    setImageList(prev => [...prev, ...imagesInUpload]);
+    setImageList(draft => draft.concat(imagesInUpload));
 
     // upload files
     files.forEach((file, index) => {
@@ -44,40 +44,34 @@ export default function Home() {
           const { loaded, total } = progressEvent;
           if (!total) { return }
           // use setState callback here to avoid stale state
-          setImageList(prev => {
-            const updated = [...prev];
-            const item = updated[i];
-            if (item.state === 'loading') {
-              item.progress = Math.round(loaded / total * 100);
+          setImageList(draft => {
+            if (draft[i].state === 'loading') {
+              draft[i].progress = Math.round(loaded / total * 100);
             }
-            return updated;
           });
         }
       }
 
       axios.post('http://localhost:3000/api/upload', formData, config).then(res => {
-        setImageList(prev => {
-          const updated = [...prev];
-          const item = updated[i];
+        setImageList(draft => {
+          const item = draft[i]
           if (item.state === 'loading') {
-            updated.splice(i, 1, {
+            draft[i] = {
               state: 'load-success',
-              id: item.title,
+              id: item.title, // use titles as id
               title: item.title,
               totalSize: item.totalSize,
               imgSrc: item.imgSrc,
               onDelete: () => { } // TODO:
-            });
+            }
           }
-          return updated;
         });
 
         setTimeout(() => {
-          setImageList(prev => {
-            const updated = [...prev];
-            const item = updated[i];
+          setImageList(draft => {
+            const item = draft[i];
             if (item.state === 'load-success') {
-              updated.splice(i, 1, {
+              draft[i] = {
                 state: 'complete',
                 id: item.title,
                 title: item.title,
@@ -85,32 +79,28 @@ export default function Home() {
                 imgSrc: item.imgSrc,
                 onCropImage: () => { }, // TODO:
                 onDelete: () => { } // TODO:
-              });
+              };
             }
-            return updated;
           })
         }, 2000);
       }).catch(err => {
         if (err instanceof AxiosError) {
-          setImageList(prev => {
-            const updated = [...prev];
-            const item = updated[i];
+          setImageList(draft => {
             let error = err.response?.data.message;
 
             if (err.code === AxiosError.ERR_NETWORK) {
               error = 'An error occurred during the upload. Please check your network connection and try again.';
             }
 
-            updated.splice(i, 1, {
+            const item = draft[i];
+            draft[i] = {
               state: 'error',
               id: item.title,
               title: item.title,
               totalSize: item.totalSize,
               onDelete: () => { }, // TODO:
               error
-            });
-
-            return updated;
+            }
           })
         }
       });
