dfc9e92d007d5e5b9c1f60ed10e33ac0863b111e
qianzhong516 <zhongqian516@gmail.com>
2024-07-17 13:34:28 +0200

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,157 +2,22 @@
 
 import ProfileBanner from '@/components/profile_banner/profile_banner';
 import Image from 'next/image';
-import UploadImageModal from '@/components/upload_image_modal/upload_image_modal';
 import { useCallback, useEffect, useState } from 'react';
-import { AxiosError, AxiosProgressEvent } from 'axios';
 import axios from '@/axios';
-import { getImgSrc, getTotalSize } from '@/utils';
-import { ImageListProps } from '@/components/image_list/image_list';
-import { useImmer } from "use-immer";
-import { ProfileIcons } from '@/models/ProfileIcon';
+import { createUploadImageModal } from '@/components/upload_image_modal/create';
 
 // TODO: remove this later
 const CURRENT_USER_ID = '66882ac39085ad43fb32ce05';
 
-const IMAGE_UPLOAD_LIMIT = 5;
-
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
-  const [imageList, setImageList] = useImmer<ImageListProps>([]);
-  const [hasFetchError, setHasFetchError] = useState(false);
-  const [selectedOption, setSelectedOption] = useState('');
   const [profileIcon, setProfileIcon] = useState('');
 
   const handleUpdatePicture = useCallback(() => setIsOpen(true), []);
-  const handleOnClose = useCallback(() => setIsOpen(false), []);
-  const handleOnConfirm = useCallback(async () => {
-    setIsOpen(false);
-    const res = await axios.put(`api/user/${CURRENT_USER_ID}`, {
-      data: {
-        profileIconTitle: selectedOption
-      }
-    });
-    const profileIcon = res.data.message.profileIcon;
-    if (profileIcon) {
-      setProfileIcon(profileIcon);
-    }
-  }, [selectedOption]);
-
-  const createDeleteImageHandler = useCallback((fileName: string) => async () => {
-    setImageList(draft => draft.filter(d => d.title !== fileName));
-    await axios.delete(`api/profile-icons/${fileName}`, {
-      data: {
-        uploadedBy: CURRENT_USER_ID
-      }
-    }).catch(err => {
-      // TODO: show the error in a toast
-      console.log(err);
-    });
-  }, [setImageList]);
-
-  const createCancelUploadHandler = useCallback((fileName: string, controller: AbortController) => () => {
-    setImageList(draft => draft.filter(d => d.title !== fileName));
-    controller.abort('Cancel upload.');
-  }, [setImageList]);
-
-  const handleUploadImage = async (files: File[]) => {
-    // read image baseUrls in parallel
-    const sources = await getImgSrc(files);
-
-    files.forEach((file, index) => {
-      const i = imageList.length + index;
-      const title = files[index].name; // TODO: deal with duplicate file name
-      const imgSrc = sources[index] as string;
-      const totalSize = getTotalSize(file.size);
-
-      const controller = new AbortController();
-      setImageList(draft => {
-        draft[i] = {
-          state: 'loading',
-          id: title,
-          title,
-          imgSrc,
-          totalSize,
-          progress: 0,
-          onCancelUpload: createCancelUploadHandler(title, controller)
-        }
-      });
-
-      const formData = new FormData();
-      formData.append('file', file);
-      const config = {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        },
-        onUploadProgress: (progressEvent: AxiosProgressEvent) => {
-          const { loaded, total } = progressEvent;
-          if (!total) { return }
-          // use setState callback here to avoid stale state
-          setImageList(draft => {
-            if (draft[i].state === 'loading') {
-              draft[i].progress = Math.round(loaded / total * 100);
-            }
-          });
-        },
-        signal: controller.signal
-      }
-
-      axios.post('/api/upload', formData, config).then(res => {
-        setImageList(draft => {
-          const item = draft[i]
-          draft[i] = {
-            state: 'load-success',
-            id: title, // use titles as id
-            title,
-            totalSize,
-            imgSrc,
-            onDelete: createDeleteImageHandler(item.title)
-          }
-        });
-
-        setTimeout(() => {
-          setImageList(draft => {
-            const item = draft[i];
-            draft[i] = {
-              state: 'complete',
-              id: title,
-              title,
-              totalSize,
-              imgSrc,
-              onChangeSelection: (e) => setSelectedOption(e.target.value),
-              onCropImage: () => { }, // TODO:
-              onDelete: createDeleteImageHandler(item.title)
-            };
-          })
-        }, 2000);
-      }).catch(err => {
-        console.log(err)
-
-        if (err instanceof AxiosError) {
-          setImageList(draft => {
-            let error = err.response?.data.message;
-
-            if (err.code === AxiosError.ERR_CANCELED) {
-              error = 'The uploading process has been cancelled';
-            }
-
-            if (err.code === AxiosError.ERR_NETWORK) {
-              error = 'An error occurred during the upload. Please check your network connection and try again.';
-            }
-
-            draft[i] = {
-              state: 'error',
-              id: title,
-              title,
-              totalSize,
-              onDelete: () => setImageList(draft => draft.filter(d => d.title !== title)),
-              error
-            }
-          })
-        }
-      });
-    });
-  };
+  const closeModal = useCallback(() => setIsOpen(false), []);
+  const updateProfileIcon = useCallback((icon: string) => {
+    setProfileIcon(icon);
+  }, []);
 
   useEffect(() => {
     async function displayUserProfileIcon() {
@@ -170,35 +35,7 @@ export default function Home() {
     displayUserProfileIcon();
   }, []);
 
-  useEffect(() => {
-    async function displayUploadedIcons() {
-      try {
-        const res = await axios.get('/api/profile-icons', {
-          params: {
-            userId: CURRENT_USER_ID
-          }
-        });
-        const icons = res.data.message || [];
-        setImageList(icons.map(({ title, totalSizeInBytes, path }: ProfileIcons, i: number) => ({
-          state: 'complete',
-          id: title,
-          title,
-          totalSize: totalSizeInBytes,
-          imgSrc: path,
-          onChangeSelection: (e: React.ChangeEvent<HTMLInputElement>) =>
-            setSelectedOption(e.target.value),
-          onCropImage: () => { }, // TODO:
-          onDelete: createDeleteImageHandler(title),
-        })));
-      } catch (_) {
-        setHasFetchError(true);
-      }
-    }
-
-    if (isOpen) {
-      displayUploadedIcons();
-    }
-  }, [createDeleteImageHandler, isOpen, setImageList]);
+  const UploadImageModal = createUploadImageModal();
 
   return (
     <div className='w-full max-w-[600px] mt-[200px] mx-auto'>
@@ -212,15 +49,11 @@ export default function Home() {
         nation='Canada'
         city='Vancouver'
         onUpdatePicture={handleUpdatePicture} />
-      {isOpen && <UploadImageModal
-        open={isOpen}
-        allowMutiple={true}
-        imageList={Array.from(imageList.values())}
-        error={imageList.length >= IMAGE_UPLOAD_LIMIT ? 'reachedLimit' : hasFetchError ? 'dataFetch' : undefined}
-        uploadFiles={handleUploadImage}
-        onClose={handleOnClose}
-        onConfirm={handleOnConfirm}
-      />}
+      <UploadImageModal
+        isOpen={isOpen}
+        closeModal={closeModal}
+        updateProfileIcon={updateProfileIcon} />
     </div>
   );
 }
+
