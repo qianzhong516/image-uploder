a8f52388faba8a7e95c99f6c59c8d2ece2028912
qianzhong516 <zhongqian516@gmail.com>
2024-07-17 16:15:27 +0200

    add image crop route handler

--- src/app/api/profile-icons/[id]/route.ts
+++ src/app/api/profile-icons/[id]/route.ts
@@ -1,7 +1,77 @@
-import { unlink } from 'fs/promises';
+import { unlink, readFile } from 'fs/promises';
 import path from 'path';
 import { NextRequest, NextResponse } from 'next/server';
 import ProfileIcon from '@/models/ProfileIcon';
+import sharp from 'sharp';
+import { promisify } from 'util';
+
+// profile-icons/[id]
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const fileId = params.id;
+  const data = await req.json();
+  const { uploadedBy: userId, cropData, cropRatio } = data;
+
+  Object.keys(cropData).forEach((key) => {
+    const value = cropData[key];
+    if (typeof value === 'number') {
+      cropData[key] = Math.round(cropRatio * value);
+    }
+  });
+
+  const inputPath = path.resolve(
+    process.cwd(),
+    `public/uploads/${userId}/${fileId}`
+  );
+
+  const newTitle = `${Date.now()}-${fileId}`;
+  const outputPath = path.resolve(
+    process.cwd(),
+    `public/uploads/${userId}/${newTitle}`
+  );
+
+  try {
+    const input = await readFile(inputPath);
+    const instance = sharp(input).extract(cropData);
+    const toFile = promisify(instance.toFile.bind(instance));
+    await toFile(outputPath);
+
+    // delete the old file
+    await unlink(inputPath);
+
+    const result = await ProfileIcon.findOneAndUpdate(
+      {
+        title: fileId,
+        uploadedBy: userId,
+      },
+      {
+        title: newTitle,
+        path: `/uploads/${userId}/${newTitle}`,
+      },
+      {
+        new: true,
+      }
+    );
+
+    return NextResponse.json(
+      {
+        message: result,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json(
+        {
+          message: err.message,
+        },
+        { status: 400 }
+      );
+    }
+  }
+}
 
 // profile-icons/[id]
 export async function DELETE(
@@ -10,13 +80,13 @@ export async function DELETE(
 ) {
   const fileId = params.id;
   const data = await req.json();
-  const { uploadedBy } = data;
+  const { uploadedBy: userId } = data;
 
   try {
     await unlink(
       path.resolve(
         process.cwd(),
-        `public/uploads/${uploadedBy}/${fileId}`
+        `public/uploads/${userId}/${fileId}`
       )
     );
     await ProfileIcon.deleteOne({
