1fdc9f6ba38a682e6bc78507a3150a5355095f58
qianzhong516 <zhongqian516@gmail.com>
2024-07-19 18:09:41 +0200

    fix public url in prod

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : process.env.NEXT_PUBLIC_URL,
 });
 
 export default instance;
