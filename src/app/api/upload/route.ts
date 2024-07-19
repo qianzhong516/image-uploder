a3d5e04b55ffed443c22f9d62d1136c2faa4d55f
qianzhong516 <zhongqian516@gmail.com>
2024-07-20 01:56:06 +0200

    add get presigned url route

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -5,9 +5,66 @@ import path from 'path';
 import z, { ZodError } from 'zod';
 import ProfileIcon from '@/models/ProfileIcon';
 import dbConnect from '@/lib/dbconnect';
+import client from '../client';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { v1 as uuid } from 'uuid';
 
 // TODO: remove this later
 const USER_ID = '66882ac39085ad43fb32ce05';
+
+export async function GET(req: NextRequest) {
+  if (!process.env.S3_BUCKET) {
+    return NextResponse.json(
+      { error: 'No bucket is specified.' },
+      { status: 500 }
+    );
+  }
+
+  const { searchParams: params } = new URL(req.url);
+  const fileName = params.get('fileName');
+
+  if (!fileName) {
+    return NextResponse.json(
+      { error: 'No fileName is specified.' },
+      { status: 500 }
+    );
+  }
+
+  const ext = path.extname(fileName);
+  const formatLimit = z.string().regex(/\.(png|jpeg|jpg)$/i, {
+    message: `The file format of ${ext} is not supported. Please upload an image in one of the following formats: JPG or PNG.`,
+  });
+  try {
+    formatLimit.parse(ext);
+  } catch (err) {
+    if (err instanceof ZodError) {
+      return NextResponse.json(
+        { message: err.issues[0].message },
+        { status: 400 }
+      );
+    }
+  }
+
+  try {
+    const key = `${USER_ID}/${uuid()}${ext}`;
+    const command = new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET,
+      Key: key,
+    });
+    const url = await getSignedUrl(client, command, {
+      expiresIn: 3600,
+    });
+    return NextResponse.json({ message: url }, { status: 200 });
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json(
+        { error: err.message },
+        { status: 500 }
+      );
+    }
+  }
+}
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
